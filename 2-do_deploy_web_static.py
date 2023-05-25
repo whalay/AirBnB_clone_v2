5766774d6e40ffd63fe1ac7c7cf1c7f6693a45c9
root <whalay>
2023-05-25 19:26:24 +0000

    added

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,7 +4,7 @@ from fabric.api import local, put, run, env
 from datetime import datetime
 from os.path import isfile
 
-env.hosts = ["54.208.44.184", "100.25.199.87"]
+env.hosts = ["54.197.113.126", "54.84.209.67"]
 env.user = "ubuntu"
 
 
